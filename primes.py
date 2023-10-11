8260691868f8df19eca407dd4aff550d7fad0c55
Sameen Ahmed <sameenahmed7047@gmail.com>
2023-10-11 17:05:50 +0100

    Complete task by raising an exception if the inputted value is less than or equal to 0.

--- primes.py
+++ primes.py
@@ -2,5 +2,27 @@
 """ENTER YOUR SOLUTION HERE!"""
 
 def primes(number_of_primes):
-    list = []
+    if number_of_primes <= 0:
+        raise ValueError("Number of primes must be a positive integer")
+    
+    prime_list = []
+    num = 2
+    while len(prime_list) < number_of_primes:
+        if is_prime(num):
+            prime_list.append(num)
+        num += 1
+    print(prime_list)
     return list
+
+def is_prime(n):            # checks if a number is prime, returns True if it is and False if not
+    if n == 2 or n == 3:
+        return True
+    elif n <= 1 or n % 2 == 0 or n % 3 == 0:
+        return False
+
+    i = 5
+    while i * i <= n:
+        if n % i == 0 or n % (i + 2) == 0:
+            return False
+        i += 6
+    return True
