069e298d4f51c8ced7a6a765ba6bf54896907f25
Sameen Ahmed <sameenahmed7047@gmail.com>
2023-10-11 17:30:42 +0100

    Final.

--- primes.py
+++ primes.py
@@ -11,15 +11,15 @@ def primes(number_of_primes):
         if is_prime(num):
             prime_list.append(num)
         num += 1
-    print(prime_list)
-    return list
+    return prime_list
 
-def is_prime(n):            # checks if a number is prime, returns True if it is and False if not
-    if n == 2 or n == 3:
-        return True
-    elif n <= 1 or n % 2 == 0 or n % 3 == 0:
+def is_prime(n):
+    if n <= 1:
         return False
-
+    if n <= 3:
+        return True
+    if n % 2 == 0 or n % 3 == 0:
+        return False
     i = 5
     while i * i <= n:
         if n % i == 0 or n % (i + 2) == 0:
